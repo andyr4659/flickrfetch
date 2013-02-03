29098fa64d12f52bc4d27cbdbb5138419238c16f
andy rosenbaum <andyr4659@yahoo.com>
2013-02-03 11:28:37 -0500

    v1
    final cut for demo

---
+++ flickrfetchdemo/script/ff.js
@@ -0,0 +1,14 @@
+﻿function loadImages() {
+    $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?',
+        function (data) {
+            $.each(data.items, function (i, item) {
+                $("<img/>").attr({
+                    "src": item.media.m,
+                    "title": item.title,
+                    "alt": item.title
+                }).prependTo("#flimages").wrap("<a href='" + item.media.m + "'></a>");
+                $.getScript('script/html5gallery.js'), function (data, textStatus, jqxhr) { };
+                document.getElementById("imgLoader").style.display = "none";
+            });
+        })
+}
